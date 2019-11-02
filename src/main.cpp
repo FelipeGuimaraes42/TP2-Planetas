d43192e72e3904f97f99d50337169859241cc810
Felipe Guimarães <felipeguima4@gmail.com>
2019-11-01 23:11:38 -0300

    Subvetores consertados!

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,10 @@ int main() {
   printf("Inicio!\n");
   while(scanf("%d %d %d", &t, &p, &c) != EOF) {
     Planeta vetor[p];
-    int meses, cursor, e;
-    e= cursor= meses= 0;
+    int meses, cursor, e, i;
+    i= e= cursor= meses= 0;
     int visitas_mes= 0;
-    int cont= 1;
+    int cont= 0;
     for(int i=0; i<p; i++){
       //scanf("%d %s", &vetor[i].tempo, &vetor[i].nome);
       std::cin >> vetor[i].tempo >> vetor[i].nome;
@@ -35,10 +35,11 @@ int main() {
     std::cout << meses << std::endl;
 
     if(meses>1){
-      for(int i= 0; i<p; i++){
-        if((visitas_mes+ vetor[i].tempo)<= t){
+      /*for(i= 0; i<=p; i++){
+        if(((visitas_mes+ vetor[i].tempo)<= t) && (i<p)){
           visitas_mes+= vetor[i].tempo;
         }else{
+          visitas_mes= 0;
           Planeta *aux= new Planeta[i];
           for(int j=0; j<i-e; j++){
             aux[j]= vetor[cursor];
@@ -48,9 +49,29 @@ int main() {
           e= cursor;
           //RADIX e copiar no vetor principal
           cont++;
-          for(int j=0; j<i; j++){
+          for(int j=0; j<=p-e; j++){
             std::cout << aux[j].mes << " " << aux[j].nome << " " << aux[j].tempo << std::endl;
           }
+        }
+      }*/
+      while(cont< meses){
+        if(((visitas_mes+ vetor[i].tempo)<= t) && (i<p)){
+          visitas_mes+= vetor[i].tempo;
+          i++;
+        }else{
+          visitas_mes= 0;
+          Planeta *aux= new Planeta[i-e];
+          for(int j=0; j<i-e; j++){
+            aux[j]= vetor[cursor];
+            aux[j].mes= cont+1;
+            cursor++;
+          }
+          //RADIX e copiar no vetor principal
+          cont++;
+          for(int j=0; j<i-e; j++){
+            std::cout << aux[j].mes << " " << aux[j].nome << " " << aux[j].tempo << std::endl;
+          }
+          e= cursor;
         }
       }
     }else{
@@ -58,5 +79,6 @@ int main() {
     }
 
   }
+
   return 0;
 }
